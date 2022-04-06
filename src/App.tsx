afa19655f6cf35ffe227c1848c5559bf149f7bb9
Toan Nguyen <toan.nguyen@hitatek.io>
2022-04-06 20:37:00 +0700

    init basic components

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,23 @@ import logo from './logo.png';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import configureStore from './configureStore';
+import Global from './styles/global';
+import Introduction from './components/Introduction';
+import MainPage from './components/MainPage';
+import Navbar from './components/Navbar';
 
 function App() {
     const store = configureStore();
 
     return (
         <>
+            <Global />
             <Provider store={store}>
+                <Introduction />
                 <BrowserRouter>
-                    <div className="App">
-                        <img src={logo} className="App-logo" alt="logo" />
-                    </div>
+                    <MainPage />
                 </BrowserRouter>
+                <Navbar />
             </Provider>
         </>
     );
